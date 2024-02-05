1633e027f5c80f4dc54606a8b7bee7a3e7fe6014
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2024-02-05 18:24:31 -0500

    Create q341.py

---
+++ q341.py
@@ -0,0 +1,44 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    
+    def __init__(self, nestedList: [NestedInteger]):
+        self.stack = list(reversed(nestedList))
+        
+        
+    def next(self) -> int:
+        self.make_stack_top_an_integer()
+        return self.stack.pop().getInteger()
+    
+        
+    def hasNext(self) -> bool:
+        self.make_stack_top_an_integer()
+        return len(self.stack) > 0
+        
+        
+    def make_stack_top_an_integer(self):
+        # While the stack contains a nested list at the top...
+        while self.stack and not self.stack[-1].isInteger():
+            # Unpack the list at the top by putting its items onto
+            # the stack in reverse order.
+            self.stack.extend(reversed(self.stack.pop().getList()))
